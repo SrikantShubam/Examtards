8fb4f9209f1c04d4d4335f595d01eca8a17f6f6d
Tabish Khalid Halim <tkhalim14@gmail.com>
2024-02-25 14:51:52 +0530

    Update tool.py

--- logistic_classifier/tool.py
+++ logistic_classifier/tool.py
@@ -4,9 +4,10 @@ import pytesseract
 from pytesseract import Output
 import re
 from io import BytesIO
+import joblib
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
+model = joblib.load('./random_forest_model.pkl')
 
 def extract_questions_and_options(text):
     # Define regular expressions for questions and options
@@ -25,6 +26,11 @@ def extract_questions_and_options(text):
 
     return result
 
+def extract_features(image):
+    hist = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+    hist = cv2.normalize(hist, hist).flatten()  # Normalize and flatten the histogram
+    return hist
+
 def extract_text_from_image(image_path):
     # Read the image using OpenCV
     image = cv2.imread(image_path)
@@ -39,7 +45,8 @@ def extract_text_from_image(image_path):
     ### Image to sub-parts , contours with margin around text
     contours, _ = cv2.findContours(threshold_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    extracted_text = []
+    questions = []
+    options = []
 
     # for cropping for sub-page
 
@@ -57,14 +64,30 @@ def extract_text_from_image(image_path):
         # Crop the image around the bounding box
         cropped_image = image[y:y+h, x:x+w]
 
+        feature_vector = extract_features(cropped_image)
+
+        prediction = model.predict([feature_vector])
+        # print(prediction)
+
         # Use pytesseract to perform OCR on the cropped image
         custom_config = r'--oem 3 --psm 6'  # OCR Engine Mode (OEM) 3 for both standard and LSTM OCR, Page Segmentation Mode (PSM) 6 for sparse text
         text = pytesseract.image_to_string(cropped_image, config=custom_config)
 
-        # Append the extracted text to the list
-        extracted_text.append(text)
 
-    return '\n'.join(extracted_text)
+        # Append the extracted text to the list
+        if(prediction[0]==1):
+            questions.append(text)
+        else:
+            options.append(text)
+
+    extracted_text = {
+        'questions': questions,
+        'options': options
+    }
+
+    print(extracted_text)
+
+    return extracted_text
 
 def extract_data_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as file:
@@ -97,8 +120,7 @@ def extract_data_from_pdf(pdf_path):
 
                 extracted_data.append({
                     'page_num': page_num + 1,
-                    'text': text,
-                    'image_snippets': image_snippets
+                    'text': image_snippets
                 })
             else:
                 extracted_data.append({
@@ -108,15 +130,23 @@ def extract_data_from_pdf(pdf_path):
 
     return extracted_data
 
-def main(file_name, destination_path="."):
+def main(file_name):
     pdf_path = file_name  # Update with the path to your PDF file
     extracted_data = extract_data_from_pdf(pdf_path)
-    file_name=file_name.split("/")[-1]
 
     import json
     json.dumps(extracted_data, indent=4)
-    with open(f'{destination_path}/{file_name[:-4]}.json', 'w') as file:
+    with open(f'./{file_name[:-4]}.json', 'w') as file:
         json.dump(extracted_data, file, indent=4)
+
+    # for data in extracted_data:
+    #     print(f"Page {data['page_num']}:")
+    #     print("Text:")
+    #     print(data['text'])
+    #     if 'image_snippets' in data:
+    #         print("Image Snippets:")
+    #         for image_snippet in data['image_snippets']:
+    #             print(f" - {image_snippet}")
 
 if __name__ == "__main__":
     import sys
